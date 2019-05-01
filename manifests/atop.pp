3505b1e3384cb9b161620bc679e56fbd40b0cca5
Ali Dogru <ali_ms_test@outlook.com>
2019-05-01 10:46:01 +0000

    htop out atop in 600

---
+++ manifests/atop.pp
@@ -0,0 +1,15 @@
+# Set up regular Puppet runs
+file { '/tmp/AAAAA.txt':
+  ensure => present,
+  content => 'AAAAAAAAAAAAAAAAAAAAAA',
+  owner  => 'ubuntu',
+  mode   => '0600',
+}
+
+package { 'atop':
+  ensure => 'installed',
+}
+ 
+package { 'htop':
+  ensure => 'absent',
+}
