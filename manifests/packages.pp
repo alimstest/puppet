be485a4e571d8a129d2361abdd102d3c2b4c529a
Ali Dogru <ali_ms_test@outlook.com>
2019-05-01 14:07:35 +0000

    Syntax Error fixed

--- manifests/packages.pp
+++ manifests/packages.pp
@@ -1,9 +1,9 @@
 # Set up regular Puppet runs
 file { '/tmp/AAAAA.txt':
-  ensure => present,
+  ensure  => present,
   content => 'AAAAAAAAAAAAAAAAAAAAAA',
-  owner  => 'vagrant',
-  mode   => '0777',
+  owner   => 'vagrant',
+  mode    => '0777',
 }
 
 $needs = [ 'tmux', 'sysdig', 'atop', 'htop', 'dstat', 'vlan', 'puppet-lint' , ]
@@ -12,7 +12,3 @@ package { $needs:
   ensure => 'installed',
 }
 
-#package { 'puppet-lint':
-#  ensure   => '1.1.0',
-#  provider => 'gem',
-#} 
